3e43554c01185fa9d715abb1fb055ad0ff068a8f
solofo <solofo.ralitera@gmail.com>
2017-06-22 21:01:11 +0300

    Clean code

--- src/ApiBundle/Services/CommentService.php
+++ src/ApiBundle/Services/CommentService.php
@@ -2,21 +2,6 @@
 
 namespace ApiBundle\Services;
 
-use Doctrine\ORM\EntityManager;
-use Symfony\Component\HttpFoundation\RequestStack;
-
 class CommentService
 {
-    private $em;
-    private $requestStack;
-
-    public function __construct(
-        RequestStack $requestStack,
-        EntityManager $entityManager
-    )
-    {
-        $this->em = $entityManager;
-        $this->requestStack = $requestStack;
-    }
-
 }
